8f898d69906d321362f982695dc8a7e72f65560b
Anne <annemwaniki94@gmail.com>
2019-05-14 10:53:26 +0300

    add alert when player rolls 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+function Player () {
+  this.dice1 = 0;
+  this.dice2 = 0;
+  this.ongoingScore = 0;
+  this.totalScore = 0;
+};
+
 function roll1() {
-  var x = Math.floor(Math.random() * 6) + 1;
-  document.getElementById('score1').innerHTML = x;
+  var dice1 = Math.floor(Math.random() * 6) + 1;
+  document.getElementById('score1').innerHTML = dice1;
+  if (dice1 === 1) {
+    alert("You rolled 1! Hold for the next player.")
+  } else {
+    this.ongoingScore += dice1;
+  }
+  return dice1;
 }
 
 function roll2() {
-  var y = Math.floor(Math.random() * 6) + 1;
-  document.getElementById('score2').innerHTML = y;
+  var dice2 = Math.floor(Math.random() * 6) + 1;
+  document.getElementById('score2').innerHTML = dice2;
+  if (dice2 === 1) {
+    alert("You rolled 1! Hold for the next player.")
+  } else {
+    this.ongoingScore += dice2;
+  }
+  return dice2;
 }
